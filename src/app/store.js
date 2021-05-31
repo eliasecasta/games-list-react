dc5441ac85d974530769edc09a8e4c8e9701de69
Elias Castañeda <e71az@icloud.com>
2021-05-31 12:16:20 -0600

    Add: Redux persist package to prevent state from being lost at refresh

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 import gameReducer from '../reducers/gameSlice';
 
+const reducers = combineReducers({
+  game: gameReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    game: gameReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import store from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './router/Router';
 import './style/style.css';
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
