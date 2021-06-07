0ed83fdf8bf1a150c00298c2bbe5cf00dce908f0
Elias Castañeda <e71az@icloud.com>
2021-06-07 11:57:42 -0600

    Edit: Remove console.log

--- src/reducers/gameSlice.js
+++ src/reducers/gameSlice.js
@@ -44,7 +44,6 @@ export const fetchFavourites = createAsyncThunk(
           },
         },
       );
-      console.log(response.data);
       return response.data;
     } catch (error) {
       console.log(error);
